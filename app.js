128d0b70aa1ca91070704bd2c92c16a73563f6eb
Anil Kumar Patra <anilpatra927@gmail.com>
2023-06-19 10:41:13 +0530

    second commit

--- app.js
+++ app.js
@@ -122,6 +122,6 @@ app.get("/:customListName", async function(req,res){
 })
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("server is running at port 3000");
 })
